97ff8d924719d1c34f14605936cffa348edcf00d
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-12-04 12:07:33 +0330

    create BaseEntityService

---
+++ src/main/java/base/service/BaseService.java
@@ -0,0 +1,26 @@
+package base.service;
+
+import base.entity.BaseEntity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Optional;
+
+public interface BaseService<T extends BaseEntity<ID>,ID extends Serializable>{
+
+    T saveOrUpdate(T entity);
+
+    Optional<T> findById(ID id);
+
+    void deleteById(ID id);
+
+    Collection<T> findAll();
+    boolean existsById(ID id);
+
+    void beginTransaction();
+
+    void commitTransaction();
+
+    void rollback();
+
+}
