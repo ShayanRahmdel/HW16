09d066d2daad31e9f5c717e9e2c0f13d60d0daab
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-12-04 12:07:09 +0330

    create BaseEntityRepository

---
+++ src/main/java/base/repository/BaseEntityRepository.java
@@ -0,0 +1,28 @@
+package base.repository;
+
+import base.entity.BaseEntity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Optional;
+
+public interface BaseEntityRepository<T extends BaseEntity<ID>, ID extends Serializable>{
+
+        T saveOrUpdate(T entity);
+
+        Optional<T> findById(ID id);
+
+        void deleteById(ID id);
+
+        Collection<T> findAll();
+        boolean existsById(ID id);
+
+        void beginTransaction();
+
+        void commitTransaction();
+
+        void rollBack();
+
+
+
+}
