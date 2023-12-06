6089fdb480b321a1df1f4f09ef3c63d0f208dbd8
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-12-06 15:25:43 +0330

    Add Student

--- src/main/java/util/AppContext.java
+++ src/main/java/util/AppContext.java
@@ -1,5 +1,10 @@
 package util;
 
+import repository.StudentRepository;
+import repository.impl.StudentRepositoryImpl;
+import service.StudentService;
+import service.impl.StudentServiceImpl;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -7,15 +12,24 @@ import javax.persistence.Persistence;
 public class AppContext {
     private static final EntityManagerFactory ENTITY_MANAGER_FACTORY;
     private static final EntityManager ENTITY_MANAGER;
+    private static final StudentRepository STUDENT_REPOSITORY;
+
+    private static final StudentService STUDENT_SERVICE;
 
 
     static {
         ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("default");
         ENTITY_MANAGER = ENTITY_MANAGER_FACTORY.createEntityManager();
+        STUDENT_REPOSITORY = new StudentRepositoryImpl(ENTITY_MANAGER);
+        STUDENT_SERVICE = new StudentServiceImpl(STUDENT_REPOSITORY);
     }
 
 
     public static void init() {
         System.out.println("initialize DateBase");
+    }
+
+    public static StudentService getStudentService(){
+        return STUDENT_SERVICE;
     }
 }
