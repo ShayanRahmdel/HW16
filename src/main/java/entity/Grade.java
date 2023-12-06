5e8f3e9c22cf0755e70b215e15e8de92927f04ed
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-12-06 15:26:36 +0330

    create Grade

---
+++ src/main/java/entity/Grade.java
@@ -0,0 +1,13 @@
+package entity;
+
+public enum Grade {
+
+    Associate,
+    Bachelor_Continuous,
+    Bachelor_Discontinuous,
+    Masters_Continuous,
+    Masters_Discontinuous,
+    PHD_professional,
+    PHD_Continuous,
+    PHD_Discontinuous
+}
