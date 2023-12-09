b9f2c142d98fb154be38967850f6d82f36ca7927
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-12-09 11:45:53 +0330

    create StudentSpouseService

---
+++ src/main/java/service/StudentSpouseService.java
@@ -0,0 +1,8 @@
+package service;
+
+import base.repository.BaseEntityRepository;
+import base.service.BaseService;
+import entity.StudentSpouse;
+
+public interface StudentSpouseService extends BaseService<StudentSpouse,Integer> {
+}
---
+++ src/main/java/service/impl/StudentSpouseServiceImpl.java
@@ -0,0 +1,15 @@
+package service.impl;
+
+import base.repository.impl.BaseEntityRepositoryImpl;
+import base.service.impl.BaseEntityServiceImpl;
+import entity.StudentSpouse;
+import repository.StudentSpouseRepository;
+import service.StudentSpouseService;
+
+public class StudentSpouseServiceImpl extends BaseEntityServiceImpl<StudentSpouse,Integer,StudentSpouseRepository> implements StudentSpouseService {
+    public StudentSpouseServiceImpl(StudentSpouseRepository repository) {
+        super(repository);
+    }
+
+
+}
