d4fd8edc774f51c3df99d64c727a4d641bc17c3f
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-12-09 11:44:20 +0330

    crdeate LoanService

---
+++ src/main/java/service/LoanService.java
@@ -0,0 +1,8 @@
+package service;
+
+import base.repository.BaseEntityRepository;
+import base.service.BaseService;
+import entity.Loan;
+
+public interface LoanService extends BaseService<Loan,Integer> {
+}
---
+++ src/main/java/service/impl/LoanServiceImpl.java
@@ -0,0 +1,14 @@
+package service.impl;
+
+import base.repository.impl.BaseEntityRepositoryImpl;
+import base.service.impl.BaseEntityServiceImpl;
+import entity.Loan;
+import repository.LoanRepository;
+import service.LoanService;
+
+public class LoanServiceImpl extends BaseEntityServiceImpl<Loan,Integer,LoanRepository> implements LoanService {
+
+    public LoanServiceImpl(LoanRepository repository) {
+        super(repository);
+    }
+}
