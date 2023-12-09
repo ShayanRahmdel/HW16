6e6e23702a430b236756321a589465eca7197ff1
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-12-09 11:43:51 +0330

    crdeate LoanCategoryRepository

---
+++ src/main/java/repository/LoanCategoryRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import base.repository.BaseEntityRepository;
+import entity.LoanCategory;
+
+public interface LoanCategoryRepository extends BaseEntityRepository<LoanCategory,Integer> {
+}
