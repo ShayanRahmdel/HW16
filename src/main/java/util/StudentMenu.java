6222927a539aab133327f233e5e8fc14da738f40
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-12-10 19:29:51 +0330

    create StudentMenu

---
+++ src/main/java/util/StudentMenu.java
@@ -0,0 +1,26 @@
+package util;
+
+import entity.Student;
+
+public class StudentMenu {
+    SignUpLoan signUpLoan = new SignUpLoan();
+    public void menu(Student student){
+        Boolean flag = true;
+        Integer select = 0;
+        while (flag) {
+            System.out.println("1_signUp Loan");
+            System.out.println("2_Installment");
+            System.out.println("3_Exit");
+            select = GiveInput.giveIntegerInput();
+            switch (select) {
+                case 1->signUpLoan.menu(student);
+                case 2-> System.out.println("installment");
+                case 3 ->flag=false;
+                default -> System.out.println("Wrong");
+            }
+        }
+
+    }
+
+
+}
