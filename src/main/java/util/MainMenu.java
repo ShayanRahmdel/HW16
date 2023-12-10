78a6b08fd0467a17fb5dfd30cac76ca312efa554
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-12-10 19:28:35 +0330

    Create MainMenu

---
+++ src/main/java/util/MainMenu.java
@@ -0,0 +1,42 @@
+package util;
+
+import entity.Student;
+
+import javax.persistence.NoResultException;
+
+public class MainMenu {
+    SignUpStudent signUpStudent = new SignUpStudent();
+    SignUpLoan signUpLoan = new SignUpLoan();
+
+    public void menu(){
+
+        Boolean flag = true;
+        Integer select = 0;
+        while (flag) {
+            System.out.println("1_Sign Up");
+            System.out.println("2_Sign In");
+            System.out.println("3_Exit");
+            select = GiveInput.giveIntegerInput();
+            switch (select) {
+                case 1 -> signUpStudent.signUp();
+                case 2 ->signIn();
+                case 3 ->flag=false;
+                default -> System.out.println("Wrong");
+            }
+        }
+    }
+    private void signIn(){
+        System.out.println("Enter UserName:");
+        String userName = GiveInput.giveStringInput();
+        System.out.println("Enter Password:");
+        String password = GiveInput.giveStringInput();
+
+        try {
+            Student student = AppContext.getStudentService().findByUserNameAndPassword(userName, password);
+            System.out.println("login sucsessfully");
+            signUpLoan.menu(student);
+        }catch (NoResultException e){
+            System.out.println("not found this user");
+        }
+    }
+}
