2c17f36fb290b01d03c354b733d2d3b6f622fd78
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-12-26 11:15:35 +0330

    refactor updateBalanceById

--- src/main/java/repository/impl/CreditCardRepositryImpl.java
+++ src/main/java/repository/impl/CreditCardRepositryImpl.java
@@ -26,11 +26,11 @@ public class CreditCardRepositryImpl extends BaseEntityRepositoryImpl<CreditCard
     @Override
     public void updateBalanceById(Double balance, Bank bank, Integer studentId) {
         beginTransaction();
-        String hql = "UPDATE CreditCard SET balance = :newBalance WHERE bankName = :bankName and student.id = :studentId";
+        String hql = "UPDATE CreditCard SET balance = balance + :balance WHERE bankName = :bankName and student.id = :studentId";
         try {
             beginTransaction();
             Query query = entityManager.createQuery(hql);
-            query.setParameter("newBalance", balance);
+            query.setParameter("balance", balance);
             query.setParameter("bankName", bank);
             query.setParameter("studentId", studentId);
             query.executeUpdate();
